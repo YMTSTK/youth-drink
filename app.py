539acbb1e079f9265e9bf0b3ed1d8023c1f54fba
YMTSTK <87373329+YMTSTK@users.noreply.github.com>
2023-01-04 16:28:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def predict():
     return jsonify({'error':'false','habit': str(result),'message':'Success'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
